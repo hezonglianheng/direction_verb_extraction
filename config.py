964d641cf1a9ccf5ea9d0d7a36f31bfe42d0a488
hezonglianheng <976410976@qq.com>
2026-03-28 16:00:54 +0800

    修改LTP 模型加载路径，兼容Windows操作系统和Linux

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ FILTER_BATCH_SIZE = 1000
 FILTER_WORKERS = 2
 """筛选阶段进程数，0 表示自动使用 CPU 核数"""
 
-LTP_MODEL_PATH = r"LTP_model\base2"
+LTP_MODEL_PATH = r"LTP_model/base2"
 """LTP 模型加载路径，默认从本地加载基础模型，如果需要加载本地或线上其他模型，请修改路径"""
 # LTP支持的任务：分词 cws、词性 pos、命名实体标注 ner、语义角色标注 srl、依存句法分析 dep、语义依存分析树 sdp、语义依存分析图 sdpg
 CWS = "cws"
