e0df923391241d634875597416209cd96ab6ce72
hezonglianheng <976410976@qq.com>
2026-09-14 18:20:09 +0800

    增加词表配置文件对词语进行初筛

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="句子过滤器，过滤包含动趋式的句子")
     parser.add_argument("src_path", type=str, help="输入文件或目录路径")
     parser.add_argument("-f", "--filter_config_path", type=str, default="filter_config.json5", help="筛选配置文件路径，默认为 filter_config.json5")
-    parser.add_argument("-v", "--vocab_config_path", type=str, default=None, help="动趋式词表配置文件路径，默认为 None")
+    parser.add_argument("-v", "--vocab_config_path", type=str, default=None, help="词表配置文件路径，默认为 None")
     parser.add_argument("-t", "--tgt_path", type=str, default=".", help="输出目录路径，默认为当前目录")
     args = parser.parse_args()
     main(args.src_path, args.tgt_path, args.filter_config_path, args.vocab_config_path)
